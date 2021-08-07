394537732ef5f1a22b7bfeb8404a74e264cee71c
Abeldeboch1 <abeldeboch1@gmail.com>
2021-08-07 17:37:29 -0600

    first commit

---
+++ build/precache-manifest.ebc7771c28ca06f8e5274b9d7d8af09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826ce936129c56c197cf927b9a11c96a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f602d807fc8e51e1aeb6",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "91d9eb72ffe18551d0db",
+    "url": "/static/js/2.d10d2b74.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.d10d2b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f602d807fc8e51e1aeb6",
+    "url": "/static/js/main.c697c49c.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
